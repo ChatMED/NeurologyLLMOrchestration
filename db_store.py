6e350e4e33aba155d3ba9c717cb0b6059e78d99d
Kostadin Misev <kokimisev@gmail.com>
2025-05-21 12:36:42 +0200

    dbops

--- db_store.py
+++ db_store.py
@@ -29,7 +29,7 @@ class ReportSummary(BaseModel):
     summary: str
     full_report: dict
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class PredictedNeurologyReportRecord(Base):
